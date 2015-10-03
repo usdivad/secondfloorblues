cff5cd9665877c90be58d236d3c779adfca61317
David Su <david.d.su@gmail.com>
2015-10-02 20:54:35 -0400

    some audio playing; basic styling

--- js/main.js
+++ js/main.js
@@ -2,46 +2,89 @@ var ctx = new AudioContext();
 var path = "assets/";
 var assets = [
     {
-        mp3: path+"01.mp3",
-        img: path+"01.png"
+        init: path+"01_init.mp3",
+        loop: path+"01_loop.mp3",
+        img: path+"01.png",
+        dur: 370
     },
     {
-        mp3: path+"02.mp3",
+        init: path+"02.mp3",
         img: path+"02.png"
     },
     {
-        mp3: path+"03.mp3",
+        init: path+"03.mp3",
         img: path+"03.png"
     },
     {
-        mp3: path+"04.mp3",
+        init: path+"04.mp3",
         img: path+"04.png"
     },
     {
-        mp3: path+"05.mp3",
+        init: path+"05.mp3",
         img: path+"05.png"
     },
     {
-        mp3: path+"06.mp3",
+        init: path+"06.mp3",
         img: path+"06.png"
     },
     {
-        mp3: path+"07.mp3",
+        init: path+"07.mp3",
         img: path+"07.png"
     }
 ];
 var loops = [];
+var conductor;
+var bpm = 10; // 1 beat per ms
+var dur = 370;
 
 function init() {
     console.log("hello");
+
+    // audio
     for (var i=0; i<assets.length; i++) {
         var asset = assets[i];
-        var loop = new Kino.Loop(asset["mp3"]);
-        var sheet = "<div class='sheet' id='sheet" + (i+1) + "'><h4>" + (i+1) + ":</h4><img class='sheetimg' src='" + asset["img"] + "'/></div>";
+        var asset_init = asset.init;
+        var asset_loop = asset.loop;
+        var loop;
+        if (asset_loop === undefined) {
+            loop = new Kino.Loop(asset_init);
+        }
+        else {
+            loop = new Kino.Loop(asset_init, asset_loop);
+        }
+
+        // loop.activated = false;
         loops.push(loop);
+
+        var sheet = "<div class='sheet' index='" + i + "' id='sheet" + (i+1) + "'><h4>" + (i+1) + ":</h4><img class='sheetimg' src='" + asset["img"] + "'/></div>";
         $("#sheets").append(sheet);
     }
     console.log("hoho");
+    for (var i=0; i<loops.length; i++) {
+        loops[i].activated = false;
+        // loops[i].loop.loop(1);
+    }
+    loops[0].activated = true;
+    conductor = new Kino.Conductor(bpm, dur, [0], loops, function(){}, function(){}, function(){});
+    $("#sheet1").addClass("selected");
+
+    // controls
+    for (var i=0; i<loops.length; i++) {
+        var id = "#sheet" + (i+1);
+        $(id).on("click", function() {
+            // console.log($(this));
+            var idx = parseInt($(this).attr("index"));
+            $(".sheet").removeClass("selected");
+            $(this).toggleClass("selected");
+
+            for (var j=0; j<loops.length; j++) {
+                loops[j].activated = false;
+            }
+            loops[idx].activated = true;
+            conductor.setupTransition(bpm, assets[idx].dur, [0], loops);
+        });
+    }
+
 }
 
 window.onload = function() {
